bef0d390a5eb1834ab5d511eba6c20e38dc01d1b
Aaron Franks <aaron.franks@gmail.com>
2015-02-20 10:03:27 -0800

    Make example app marginally more interesting (no alert())

--- js/entry.js
+++ js/entry.js
@@ -5,10 +5,21 @@ var d = require('jsnox')(React)
 var SimpleForm = React.createClass({
     displayName: 'SimpleForm',
 
+    getInitialState() {
+        return { counter: 0, log: [] }
+    },
+
     onSubmit(evt) {
         evt.preventDefault()
+
         var inputEl = this.refs.text.getDOMNode()
-        alert(inputEl.value)
+        var newLog = this.state.counter + ' ' + inputEl.value;
+        this.setState({
+            counter: this.state.counter += 1,
+            log: this.state.log.concat([newLog])
+        });
+
+        inputEl.focus()
         inputEl.value = ''
     },
 
@@ -16,7 +27,8 @@ var SimpleForm = React.createClass({
         return d('form', { onSubmit: this.onSubmit }, [
             d('h1', 'Simple React example'),
             d('input:text[placeholder=Enter something]@text'),
-            d('button:submit', 'Submit')
+            d('button:submit', 'Submit'),
+            this.state.log.map(l => d('p.item', {key: l}, l))
         ])
     }
 })
