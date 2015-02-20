5b088fba6b4d59400b5c1ec222874bf9c9d5802b
Aaron Franks <aaron.franks@gmail.com>
2015-02-19 23:25:40 -0800

    Trivial example with React.

--- js/entry.js
+++ js/entry.js
@@ -1,1 +1,24 @@
-console.log('hi from js7')
+var React = require('react')
+var d = require('jsnox')(React)
+
+
+var SimpleForm = React.createClass({
+    displayName: 'SimpleForm',
+
+    onSubmit(evt) {
+        evt.preventDefault()
+        var inputEl = this.refs.text.getDOMNode()
+        alert(inputEl.value)
+        inputEl.value = ''
+    },
+
+    render() {
+        return d('form', { onSubmit: this.onSubmit }, [
+            d('h1', 'Simple React example'),
+            d('input:text[placeholder=Enter something]@text'),
+            d('button:submit', 'Submit')
+        ])
+    }
+})
+
+React.render(d(SimpleForm), document.querySelector('.app'))
